ec35e9e0e1d9cc74d50a8961e2522b0ddc9087fc
Alireza.R <255861@astanait.edu.kz>
2026-02-03 20:24:05 +0500

    Alireza Rasuli, modified MenuItemRepository

--- src/edu/aitu/oop3/db/repositories/MenuItemRepository.java
+++ src/edu/aitu/oop3/db/repositories/MenuItemRepository.java
@@ -3,7 +3,7 @@ package edu.aitu.oop3.db.repositories;
 import edu.aitu.oop3.db.entities.MenuItem;
 import java.util.List;
 
-public interface MenuItemRepository {
+public interface MenuItemRepository extends Repository<MenuItem> {
     List<MenuItem> getAllMenuItems();
     MenuItem getMenuItemById(long id);
 }
