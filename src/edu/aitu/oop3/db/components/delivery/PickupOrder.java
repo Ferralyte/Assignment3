f8aa3c56c9dc724bcd1b39970364fe1c52042199
Ferralyte <troxonixite@gmail.com>
2026-02-08 23:09:29 +0600

    Add files via upload

--- src/edu/aitu/oop3/db/components/delivery/PickupOrder.java
+++ src/edu/aitu/oop3/db/components/delivery/PickupOrder.java
@@ -1,6 +1,6 @@
 package edu.aitu.oop3.db.components.delivery;
 
-public class PickupOrder implements OrderType {
+public class PickupOrder implements OrderType { 
     public String getType() {
         return "PICKUP";
     }
